9367132a1b510abfbff6c5115d790206b0e594a5
Toby Twigger <tobytwigger1@gmail.com>
2020-07-29 15:33:11 +0100

    Update ui

---
+++ src/helpers/isintegration.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = (name) => {
+  return name.substring(0, 11) === 'integration'
+}
---
+++ src/helpers/isproduct.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = (name) => {
+  return name.includes('playground') || name.includes('portal') || name.includes('control-frontend')
+}
